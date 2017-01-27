39503e0d080701e14b16b43826517079355aeb30
aingealfire <aingealfire@gmail.com>
2017-01-26 20:05:56 -0500

    Formatted individual assignment files.

--- Krisin-Dana-Denise-SNIPE/src/snipe/Snipe.java
+++ Krisin-Dana-Denise-SNIPE/src/snipe/Snipe.java
@@ -29,32 +29,28 @@ public class Snipe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+
         //player class
         Player player1 = new Player();
-        player1.setName ("Fred");
-        
+        player1.setName("Fred");
+
         System.out.println("Player: " + player1.toString());
-        
+
         //challenge class
         Challenge challengeOne = new Challenge();
-        
+
         challengeOne.setInjuryPoints(5);
         challengeOne.setMasterCodePiece("masterCodePiece");
-        
+
         String challengeInfo = challengeOne.toString();
         System.out.println(challengeInfo);
-        
+
         //item class
         Item d1 = new Item();
-        d1.setType ("USA Dossier");
-        
+        d1.setType("USA Dossier");
+
         System.out.println("You have the " + d1.toString());
-      
-     
-      
+
     }
-    
+
 }
-
-
