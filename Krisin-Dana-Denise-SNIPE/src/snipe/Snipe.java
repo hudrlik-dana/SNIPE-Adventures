a6662e737f9fd326ffe11ed1742fc70ff9f817a0
denisegoetz <denisegoetz@192.168.0.6>
2017-01-28 13:44:15 -0500

    Denise added tests for classes in the main Snipe.java program.

--- Krisin-Dana-Denise-SNIPE/src/snipe/Snipe.java
+++ Krisin-Dana-Denise-SNIPE/src/snipe/Snipe.java
@@ -6,11 +6,16 @@
 package snipe;
 
 import byui.cit260.snipe.model.Challenge;
+import byui.cit260.snipe.model.Country;
 import byui.cit260.snipe.model.Item;
+import byui.cit260.snipe.model.Map;
 import byui.cit260.snipe.model.Player;
 import byui.cit260.snipe.model.MathPuzzle;
 import byui.cit260.snipe.model.MentalChallenge;
 import byui.cit260.snipe.model.PhysicalChallenge;
+import byui.cit260.snipe.model.Place;
+import byui.cit260.snipe.model.World;
+import java.util.List;
 
 /**
  *
@@ -59,8 +64,36 @@ public class Snipe {
         d1.setType("USA Dossier");
 
         System.out.println("You have the " + d1.toString());
+        
+        // These lines are testing classes that Denise added
+        
+        Map playerMap = new Map();
+        playerMap.setNumCols(7);
+        playerMap.setNumCols(4);
+        playerMap.setVisited(true);
+        //map has a list I couldn't get to work
+        System.out.println("Map fields " + playerMap);
 
-
+        Country usaCountry = new Country();
+        usaCountry.setEntryPointCode("G1");
+        usaCountry.setEntryPointDesc("You have arrived at SNIPE Academy");
+        //country has a list i couldn't get to work
+        System.out.println("Country fields " + usaCountry);
+        
+        Place placeOne = new Place();
+        placeOne.setCountryCode("U1");
+        placeOne.setChallenge(true);
+        placeOne.setMasterCodePiece("7-15-15-4");
+        placeOne.setPlaceCode("P");
+        placeOne.setPlaceName("Washington DC");
+        placeOne.setPlaceScene("You are standing in front of...");
+        System.out.println("Place fields " + placeOne);
+        
+        World gameWorld = new World();
+        //Couldn't get the list to work and that is all world contains.
+ 
+        //End of Denise Code
+        
     }
 
 }
