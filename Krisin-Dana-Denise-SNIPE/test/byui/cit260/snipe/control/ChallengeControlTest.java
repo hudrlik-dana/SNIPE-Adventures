282793c35a26b3e32926352e5b5db812a2ee015a
Randstad <Randstad@Sharrianel>
2017-02-07 22:51:25 -0500

    Created initial Test for ChallengeControl

---
+++ Krisin-Dana-Denise-SNIPE/test/byui/cit260/snipe/control/ChallengeControlTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.snipe.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Randstad
+ */
+public class ChallengeControlTest {
+    
+    public ChallengeControlTest() {
+    }
+
+    /**
+     * Test of calcMathPuzzle2 method, of class ChallengeControl.
+     */
+    @Test
+    public void testCalcMathPuzzle2() {
+        System.out.println("calcMathPuzzle2");
+        int totalLegs = 0;
+        int totalAnimals = 0;
+        String expResult = "";
+        String result = ChallengeControl.calcMathPuzzle2(totalLegs, totalAnimals);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
