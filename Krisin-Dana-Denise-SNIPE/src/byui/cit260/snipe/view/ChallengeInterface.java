13cd6c40f2d29620cfe062f97ee8e06e013d3ba3
aingealfire <aingealfire@gmail.com>
2017-02-28 20:04:23 -0500

    Added ChallengeInterface & Challenge (in View pkg)

---
+++ Krisin-Dana-Denise-SNIPE/src/byui/cit260/snipe/view/ChallengeInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.snipe.view;
+
+/**
+ *
+ * @author aingealfire@gmail.com (new16014@byui.edu)
+ */
+public interface ChallengeInterface {
+    
+    public void displayDesc();
+    public String getInput();
+    public boolean doAction(String value);
+
+}
