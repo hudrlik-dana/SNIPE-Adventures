20119080db8d834a4e207b38d4c54281f42c25e5
aingealfire <aingealfire@gmail.com>
2017-03-09 13:29:35 -0500

    Updated Dossier.java to enum

--- Krisin-Dana-Denise-SNIPE/src/byui/cit260/snipe/model/Dossier.java
+++ Krisin-Dana-Denise-SNIPE/src/byui/cit260/snipe/model/Dossier.java
@@ -6,95 +6,45 @@
 package byui.cit260.snipe.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  *
  * @author aingealfire@gmail.com (new16014@byui.edu)
  */
-public class Dossier implements Serializable {
+public enum Dossier implements Serializable {
 
-    //class instance variables
-    private String countryCode;
-    private String challengeChoices;
-    private String hints;
-    private String countryVis;
+USA(""),
+GERMANY(""),
+ENGLAND(""),
+SPAIN(""),
+RUSSIA(""),
+FRANCE(""),
+CANADA(""),
+BRAZIL(""),
+AUSTRALIA("");
+
+//class instance variables
+    private final String countryCode;
+    private final String hints;
 
     public String getCountryCode() {
         return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getChallengeChoices() {
-        return challengeChoices;
-    }
-
-    public void setChallengeChoices(String challengeChoices) {
-        this.challengeChoices = challengeChoices;
     }
 
     public String getHints() {
         return hints;
     }
 
-    public void setHints(String hints) {
+    Dossier(String hints) {
         this.hints = hints;
+        countryCode = new String();
     }
 
-    public String getCountryVis() {
-        return countryVis;
-    }
-
-    public void setCountryVis(String countryVis) {
-        this.countryVis = countryVis;
-    }
-
-    public Dossier() {
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 73 * hash + Objects.hashCode(this.countryCode);
-        hash = 73 * hash + Objects.hashCode(this.challengeChoices);
-        hash = 73 * hash + Objects.hashCode(this.hints);
-        hash = 73 * hash + Objects.hashCode(this.countryVis);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Dossier other = (Dossier) obj;
-        if (!Objects.equals(this.countryCode, other.countryCode)) {
-            return false;
-        }
-        if (!Objects.equals(this.challengeChoices, other.challengeChoices)) {
-            return false;
-        }
-        if (!Objects.equals(this.hints, other.hints)) {
-            return false;
-        }
-        if (!Objects.equals(this.countryVis, other.countryVis)) {
-            return false;
-        }
-        return true;
-    }
+       
 
     @Override
     public String toString() {
-        return "Dossier{" + "countryCode=" + countryCode + ", challengeChoices=" + challengeChoices + ", hints=" + hints + ", countryVis=" + countryVis + '}';
+        return "Dossier{" + "countryCode=" + countryCode + ", hints=" + hints + '}';
     }
 
 }
