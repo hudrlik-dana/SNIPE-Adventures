2b4ce86b95f7d85702f3d4034d6eaf93a44e7d11
aingealfire <aingealfire@gmail.com>
2017-03-14 16:02:28 -0400

    Added basic Exception placeholders

---
+++ Krisin-Dana-Denise-SNIPE/src/byui/cit260/snipe/exceptions/MapControlException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.snipe.exceptions;
+
+/**
+ *
+ * @author aingealfire@gmail.com (new16014@byui.edu)
+ */
+public class MapControlException {
+
+}
